4e869436366c7e094ae57934692e8f6059011c95
bjac11 <163893282+bjac11@users.noreply.github.com>
2024-04-11 09:21:39 -0500

    Create Button.js
    A reusable button for React

---
+++ Button.js
@@ -0,0 +1,10 @@
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
